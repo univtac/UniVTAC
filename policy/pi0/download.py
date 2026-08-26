5b6c5d3be1173001be4449eb14a1a1ed139db802
byml-c <2014078563@qq.com>
2026-08-26 19:18:08 +0800

    init dev-sensors

--- policy/pi0/download.py
+++
@@ -1,8 +0,0 @@
-import os
-from openpi.shared import download
-
-os.environ['OPENPI_DATA_HOME'] = '/mnt/data/baijun/UniVTAC/policy/pi0/openpi'
-path = download.maybe_download(
-    "gs://openpi-assets/checkpoints/pi0_fast_base/params"
-)
-print(path)
